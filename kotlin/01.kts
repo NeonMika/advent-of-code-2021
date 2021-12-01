650dfc4f195e09d6cdf1b17557f3faa1a2c58357
Markus Weninger <markusw92@yahoo.de>
2021-12-01 22:27:52 +0100

    Day 1: Create generic readData function, beautify Star 2 by calling windowed

--- kotlin/01.kts
+++ kotlin/01.kts
@@ -1,23 +1,27 @@
 import java.io.File
 
-// Level 01, 1 star
-println(
-    File("../data/01_01.txt")
+// Helper
+fun <A> readData(day: String, stars: String, lineMapper: (String) -> A) =
+    File("../data/${day}_$stars.txt")
         .readLines()
+        .map { lineMapper(it) }
+
+// Day 01, 1 star
+fun star1(test: Boolean = false) =
+    readData("01", if (test) "test" else "01") { it.toInt() }
         .map { it.toInt() }
         .fold(-1 to -1) { res, next -> next to res.second + if (next > res.first) 1 else 0 }
         .second
-)
 
-// Level 01, 2 start
-println(
-    File("../data/01_02.txt")
-        .readLines()
-        .map { it.toInt() }
-        .run {
-            dropLast(2).foldIndexed(mutableListOf<Int>()) { i, list, _ ->
-                list.also { it += subList(i, i + 3).sum() }
-            }
-        }
-        .fold(-1 to -1) { res, next -> next to res.second + if (next > res.first) 1 else 0 }
-        .second)
+println(star1(true))
+println(star1())
+
+// Day 01, 2 stars
+fun star2(test: Boolean = false) =
+    readData("01", if (test) "test" else "01") { it.toInt() }
+        .windowed(3) { it.sum() }
+        .windowed(2) { it[1] > it[0] }
+        .count { it }
+
+println(star2(true))
+println(star2())
