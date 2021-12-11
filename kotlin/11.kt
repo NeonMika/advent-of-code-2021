08accaad66613aaa4af4361c72e7a3ec3ec1804f
Markus Weninger <markusw92@yahoo.de>
2021-12-11 21:09:21 +0100

    Day 11: Reduce code duplication

--- kotlin/11.kt
+++ kotlin/11.kt
@@ -4,62 +4,48 @@ class Day11 : Day<TwoDimensionalArray<Int>, TwoDimensionalArray<Int>>("11") {
 
     override fun dataStar2(lines: List<String>): TwoDimensionalArray<Int> = dataStar1(lines)
 
-    override fun star1(data: TwoDimensionalArray<Int>): Number {
+    fun step(data: TwoDimensionalArray<Int>): Pair<TwoDimensionalArray<Int>, Int> {
         var cur = data
-        var incs = 0
-        repeat(100) {
-            var localIncs = 0
-            cur = cur.map { it + 1 }
-            while (cur.any { it > 9 }) {
-                for (row in 0 until cur.rows) {
-                    for (col in 0 until cur.cols) {
-                        if (cur[row, col] > 9) {
-                            cur[row, col] = Integer.MIN_VALUE
-                            localIncs++
-                            for (drow in -1..1) {
-                                for (dcol in -1..1) {
-                                    if (row + drow to col + dcol in cur) {
-                                        cur[row + drow, col + dcol]++
-                                    }
+        var localIncs = 0
+        cur = cur.map { it + 1 }
+        while (cur.any { it > 9 }) {
+            for (row in 0 until cur.rows) {
+                for (col in 0 until cur.cols) {
+                    if (cur[row, col] > 9) {
+                        cur[row, col] = Integer.MIN_VALUE
+                        localIncs++
+                        for (neighborRow in row - 1..row + 1) {
+                            for (neighborCol in col - 1..col + 1) {
+                                if (neighborRow to neighborCol in cur) {
+                                    cur[neighborRow, neighborCol]++
                                 }
                             }
                         }
                     }
                 }
             }
-            cur = cur.map { if (it < 0) 0 else it }
-            incs += localIncs
+        }
+        return cur.map { if (it < 0) 0 else it } to localIncs
+    }
+
+    override fun star1(data: TwoDimensionalArray<Int>): Number {
+        var cur = data
+        var incs = 0
+        repeat(100) {
+            step(cur).let { (stepCur, stepIncs) ->
+                cur = stepCur
+                incs += stepIncs
+            }
         }
         return incs
     }
 
     override fun star2(data: TwoDimensionalArray<Int>): Number {
         var cur = data
-        var incs = 0
         repeat(1_000_000) { runIndex ->
-            var localIncs = 0
-            cur = cur.map { it + 1 }
-            while (cur.any { it > 9 }) {
-                for (row in 0 until cur.rows) {
-                    for (col in 0 until cur.cols) {
-                        if (cur[row, col] > 9) {
-                            cur[row, col] = Integer.MIN_VALUE
-                            localIncs++
-                            for (drow in -1..1) {
-                                for (dcol in -1..1) {
-                                    if (row + drow to col + dcol in cur) {
-                                        cur[row + drow, col + dcol]++
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-            cur = cur.map { if (it < 0) 0 else it }
-            incs += localIncs
-            if (localIncs == 100) {
-                return runIndex + 1
+            step(cur).let { (stepCur, stepIncs) ->
+                cur = stepCur
+                if (stepIncs == 100) return runIndex + 1
             }
         }
         return -1
